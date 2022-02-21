055f44cc080c762dc56de02a95f6eb9e0d24ca57
Juma81 <jonathanumana10@gmail.com>
2022-02-21 14:09:10 -0600

    first-props

--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -1,25 +1,18 @@
-const TableBody = () => {
-    return (
-        <tbody>
-                    <tr>
-                        <td>Mac</td>
-                        <td>Bouncer</td>
-                    </tr>
-                    <tr>
-                        <td>Dee</td>
-                        <td>Aspiring actress</td>
-                    </tr>
-                    <tr>
-                        <td>Charlie</td>
-                        <td>Janitor</td>
-                    </tr>
-                    <tr>
-                        <td>Dennis</td>
-                        <td>Bartender</td>
-                    </tr>
-                </tbody>
-    );
-};
+const TableBody = props => {
+    const characters = props.characters.map((char, index) => {
+        return (
+                <tr key={index}>
+                    <td>
+                        { char.name }
+                    </td>
+                    <td>{ char.job }</td>
+                </tr>
+            );
+        });
+        return <tbody>{characters}</tbody>
+    };
+    
+    export default TableBody;
 
-export default TableBody;
+    
 
